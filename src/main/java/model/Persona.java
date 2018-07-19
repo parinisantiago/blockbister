17670cc7b1279f1fa9eae2c09c7269914d98d6a3
Santiago Parini <santiago.parini@globallogic.com>
2018-07-19 16:20:19 -0300

    initial commit

---
+++ src/main/java/model/Persona.java
@@ -0,0 +1,69 @@
+package model;
+
+
+public class Persona {
+
+	
+	private long id;
+	
+	private int dni;
+	
+	private String nombre;
+	
+	private int telefono;
+	
+	private String mail;
+	
+	private String direccion;
+	
+	public Persona(int dni, String nombre, int telefono, String mail, String direccion) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.mail = mail;
+		this.direccion = direccion;
+	}
+	
+	public Persona() {
+		super();
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+}
