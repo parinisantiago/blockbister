1b6947c2893305d558c25b7435b74f794adf660c
Santiago Parini <santiago.parini@globallogic.com>
2018-07-20 11:10:49 -0300

    Modelo de tipoPelicula

---
+++ src/main/java/model/TipoPelicula.java
@@ -0,0 +1,39 @@
+package model;
+
+public class TipoPelicula {
+
+	private long id;
+	private String tipo;
+	private float precio;
+	
+	public TipoPelicula(String tipo, float precio) {
+		super();
+		this.tipo = tipo;
+		this.precio = precio;
+	}
+
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+	
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public float getPrecio() {
+		return precio;
+	}
+	
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+
+}
