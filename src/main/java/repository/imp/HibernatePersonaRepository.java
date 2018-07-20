d2e00cc9ad31291848cab07cd2d3fd837721d29f
Santiago Parini <santiago.parini@globallogic.com>
2018-07-20 16:13:27 -0300

    HibernatePersonaRepository

---
+++ src/main/java/repository/imp/HibernatePersonaRepository.java
@@ -0,0 +1,13 @@
+package repository.imp;
+
+import model.Persona;
+import repository.bi.PersonaRepositoryBI;
+
+public class HibernatePersonaRepository extends BaseHibernateRepository implements PersonaRepositoryBI{
+
+	public Persona getPersona(int dni) {
+		Persona persona = (Persona) this.getSession().createQuery("FROM Persona P WHERE P.dni="+dni).list().iterator().next();
+		return persona;
+	}
+
+}
