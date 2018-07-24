93e63ab90c271d5853599f205a71f00ce749cdf2
Santiago Parini <parinisantiago@gmail.com>
2018-07-24 19:22:16 -0300

    servicio para crear persona

--- src/main/java/service/bi/PersonaServiceBI.java
+++ src/main/java/service/bi/PersonaServiceBI.java
@@ -5,5 +5,6 @@ import dto.PersonaDTO;
 public interface PersonaServiceBI {
 
 	public PersonaDTO getPersona(String dni);
-	
+	public PersonaDTO crearPersona(String dni, String nombre,String telefono,String direccion,String mail);
+
 }
--- src/main/java/service/imp/PersonaServiceImp.java
+++ src/main/java/service/imp/PersonaServiceImp.java
@@ -10,5 +10,8 @@ public class PersonaServiceImp extends BaseServiceImp implements PersonaServiceB
 	public PersonaDTO getPersona(String dni) {
 		return DTOFactory.getInstance().getPersonaDTO(RepositoryLocator.getInstance().getPersonaRepository().getPersona(dni));		
 	}
+	public PersonaDTO crearPersona(String dni, String nombre,String telefono,String direccion,String mail) {
+		return DTOFactory.getInstance().getPersonaDTO(RepositoryLocator.getInstance().getPersonaRepository().getPersona(dni));		
+	}
 	
 }
