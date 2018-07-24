4fbe364edb33ac13cff172951c828e8843b62066
Santiago Parini <parinisantiago@gmail.com>
2018-07-24 19:26:39 -0300

    Ya no se extiende de baseSErvice

--- src/main/java/service/imp/PersonaServiceImp.java
+++ src/main/java/service/imp/PersonaServiceImp.java
@@ -5,7 +5,7 @@ import dto.PersonaDTO;
 import repository.RepositoryLocator;
 import service.bi.PersonaServiceBI;
 
-public class PersonaServiceImp extends BaseServiceImp implements PersonaServiceBI{
+public class PersonaServiceImp implements PersonaServiceBI{
 
 	public PersonaDTO getPersona(String dni) {
 		return DTOFactory.getInstance().getPersonaDTO(RepositoryLocator.getInstance().getPersonaRepository().getPersona(dni));		
