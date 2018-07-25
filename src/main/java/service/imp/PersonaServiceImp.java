5d9220add41e303817f85dabcaadf610a74e0dd4
Santiago Parini <parinisantiago@gmail.com>
2018-07-24 23:04:33 -0300

    implementacion de agregarPersona para el sercivio de persona

--- src/main/java/service/imp/PersonaServiceImp.java
+++ src/main/java/service/imp/PersonaServiceImp.java
@@ -1,17 +1,21 @@
 package service.imp;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import dto.DTOFactory;
 import dto.PersonaDTO;
+import model.Persona;
 import repository.RepositoryLocator;
 import service.bi.PersonaServiceBI;
-
+@Transactional
 public class PersonaServiceImp implements PersonaServiceBI{
-
+	
 	public PersonaDTO getPersona(String dni) {
 		return DTOFactory.getInstance().getPersonaDTO(RepositoryLocator.getInstance().getPersonaRepository().getPersona(dni));		
 	}
-	public PersonaDTO crearPersona(String dni, String nombre,String telefono,String direccion,String mail) {
-		return DTOFactory.getInstance().getPersonaDTO(RepositoryLocator.getInstance().getPersonaRepository().getPersona(dni));		
+	
+	public PersonaDTO crearPersona(Persona persona) {
+		return DTOFactory.getInstance().getPersonaDTO(RepositoryLocator.getInstance().getPersonaRepository().crearPersona(persona));		
 	}
 	
 }
