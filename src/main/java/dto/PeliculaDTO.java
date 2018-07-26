8fa17928c701bff33b62b9fb709b696ad3d395b3
Santiago Parini <parinisantiago@gmail.com>
2018-07-25 22:14:11 -0300

    PeliculaDTO

---
+++ src/main/java/dto/PeliculaDTO.java
@@ -0,0 +1,58 @@
+package dto;
+
+import java.util.Date;
+
+import com.google.gson.annotations.Expose;
+
+import model.Pelicula;
+
+public class PeliculaDTO {
+	
+	private long id;
+	@Expose
+	private String titulo;
+	@Expose
+	private String sinopsis;
+	@Expose
+	private Date estreno;
+	
+	public PeliculaDTO(Pelicula pelicula) {
+		super();
+		this.id = pelicula.getId();
+		this.titulo = pelicula.getTitulo();
+		this.sinopsis = pelicula.getSinopsis();
+		this.estreno = pelicula.getEstreno();
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	public String getSinopsis() {
+		return sinopsis;
+	}
+	
+	public void setSinopsis(String sinopsis) {
+		this.sinopsis = sinopsis;
+	}
+	
+	public Date getEstreno() {
+		return estreno;
+	}
+	
+	public void setEstreno(Date estreno) {
+		this.estreno = estreno;
+	}
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+}
