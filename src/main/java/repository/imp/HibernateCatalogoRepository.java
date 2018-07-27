15a4e361d74ce0ed66f6a3f1ea9a39db169fb66f
Santiago Parini <santiago.parini@globallogic.com>
2018-07-27 16:53:49 -0300

    Implementacion de repositorio de catalogo

---
+++ src/main/java/repository/imp/HibernateCatalogoRepository.java
@@ -0,0 +1,32 @@
+package repository.imp;
+
+import repository.bi.CatalogoRepositoryBI;
+
+import java.util.Iterator;
+
+import model.Catalogo;
+import model.Pelicula;
+import model.TipoPelicula;
+import model.Blockbister;
+
+public class HibernateCatalogoRepository extends BaseHibernateRepository implements CatalogoRepositoryBI{
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void crearCatalogo(Pelicula pelicula) {
+		Iterator<TipoPelicula> tipoPeliculas = this.getSession().createQuery("FROM TipoPersona").list().iterator();
+		Blockbister blockbister = (Blockbister) this.getSession().createQuery("FROM Blockbister").list().iterator().next();
+		while(tipoPeliculas.hasNext()) {
+			TipoPelicula tipoPelicula = tipoPeliculas.next();
+			Catalogo catalogo = new Catalogo(pelicula, tipoPelicula, 0);
+			blockbister.addCatalogo(catalogo);
+		}
+	}
+	
+	public Catalogo modificarCatalogo(Catalogo catalogoJSON) {
+		Catalogo catalogo = (Catalogo) this.getSession().createQuery("FROM Persona P WHERE P.id="+catalogoJSON.getId()).list().iterator().next();
+		catalogo.setCantidad(catalogoJSON.getCantidad());
+		return catalogo;
+	}
+
+}
