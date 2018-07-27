53eb8faf48c9ba980b980ce0dd7f14c7b6a0c4c1
Santiago Parini <parinisantiago@gmail.com>
2018-07-26 22:28:56 -0300

    pelicula servie

---
+++ src/main/java/service/bi/PeliculaServiceBI.java
@@ -0,0 +1,13 @@
+package service.bi;
+
+import java.util.Collection;
+
+import dto.PeliculaDTO;
+import model.Pelicula;
+
+public interface PeliculaServiceBI {
+	public PeliculaDTO getPelicula(String titulo);
+	public PeliculaDTO crearPelicula(Pelicula pelicula);
+	public PeliculaDTO modificarPelicula(Pelicula pelicula);
+	public Collection<PeliculaDTO> getPeliculas();
+}
