6c799bc81b34c41db9242fb85cacda133966ef4d
Santiago Parini <parinisantiago@gmail.com>
2018-08-04 18:33:23 -0300

    HibernateTipoPeliculaRepository

---
+++ src/main/java/repository/imp/HibernateTipoPeliculaRepository.java
@@ -0,0 +1,22 @@
+package repository.imp;
+
+import model.Blockbister;
+import model.TipoPelicula;
+import repository.bi.TipoPeliculaRepositoryBI;
+
+public class HibernateTipoPeliculaRepository extends BaseHibernateRepository implements TipoPeliculaRepositoryBI {
+
+	@Override
+	public TipoPelicula getTipoPelicula(String id) {
+		TipoPelicula tipoPelicula = (TipoPelicula) this.getSession().createQuery("FROM TipoPelicula tp WHERE tp.id =" + id).list().iterator().next();
+		return tipoPelicula;
+	}
+
+	@Override
+	public TipoPelicula nuevo(TipoPelicula tipoPelicula) {
+		Blockbister blockbister = (Blockbister) this.getSession().createQuery("FROM Blockbister").list().iterator().next();
+		blockbister.addTipoPelicula(tipoPelicula);
+		return tipoPelicula;
+	}
+
+}
