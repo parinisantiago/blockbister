8603d5d350effd58faebe83a9535a711b7328f9f
Santiago Parini <parinisantiago@gmail.com>
2018-08-13 22:09:21 -0300

    PersonaServiceBI

--- src/main/java/service/bi/PersonaServiceBI.java
+++ src/main/java/service/bi/PersonaServiceBI.java
@@ -8,7 +8,8 @@ import model.Persona;
 public interface PersonaServiceBI {
 
 	public PersonaDTO getPersona(String dni);
-	public PersonaDTO crearPersona(Persona persona);
+	public PersonaDTO crearPersona(Persona persona) throws Exception;
 	public PersonaDTO modificarPersona(Persona persona);
 	public Collection<PersonaDTO> getPersonas();
+	public PersonaDTO borrarPersona(int dni);
 }
