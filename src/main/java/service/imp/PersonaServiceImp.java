46fc8ac000ad72916732d5901ed4d4ea5d49b154
Santiago Parini <parinisantiago@gmail.com>
2018-08-13 22:10:29 -0300

    PersonaServiceImp

--- src/main/java/service/imp/PersonaServiceImp.java
+++ src/main/java/service/imp/PersonaServiceImp.java
@@ -9,23 +9,34 @@ import dto.PersonaDTO;
 import model.Persona;
 import repository.RepositoryLocator;
 import service.bi.PersonaServiceBI;
+
 @Transactional
 public class PersonaServiceImp implements PersonaServiceBI{
 	
+	@Override
 	public PersonaDTO getPersona(String dni) {
 		return DTOFactory.getInstance().getPersonaDTO(RepositoryLocator.getInstance().getPersonaRepository().getPersona(dni));		
 	}
 	
-	public PersonaDTO crearPersona(Persona persona) {
+	@Override
+	public PersonaDTO crearPersona(Persona persona) throws Exception {
 		return DTOFactory.getInstance().getPersonaDTO(RepositoryLocator.getInstance().getPersonaRepository().crearPersona(persona));		
 	}
 	
+	@Override
 	public PersonaDTO modificarPersona(Persona persona){
 		return DTOFactory.getInstance().getPersonaDTO(RepositoryLocator.getInstance().getPersonaRepository().modificarPersona(persona));
 	}
 	
+	@Override
 	public Collection<PersonaDTO> getPersonas(){
 		return DTOFactory.getInstance().getBlockbisterDTO(RepositoryLocator.getInstance().getBlockbisterRepository().getBlockbister()).getPersonas();
 	}
 
+	@Override
+	public PersonaDTO borrarPersona(int dni){
+		return DTOFactory.getInstance().getPersonaDTO(RepositoryLocator.getInstance().getPersonaRepository().borrarPersona(dni));
+	}
+		
+	
 }
