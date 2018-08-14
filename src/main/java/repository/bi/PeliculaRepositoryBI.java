9faea7bfcdab9ad3d191f5b07f33f292b3ef462d
Santiago Parini <parinisantiago@gmail.com>
2018-08-13 22:07:54 -0300

    PeliculaREpositoryBI

--- src/main/java/repository/bi/PeliculaRepositoryBI.java
+++ src/main/java/repository/bi/PeliculaRepositoryBI.java
@@ -4,6 +4,6 @@ import model.Pelicula;
 
 public interface PeliculaRepositoryBI {
 	public Pelicula getPelicula(String titulo);
-	public Pelicula crearPelicula(Pelicula pelicula);
+	public Pelicula crearPelicula(Pelicula pelicula) throws Exception;
 	public Pelicula modificarPelicula(Pelicula pelicula);
 }
