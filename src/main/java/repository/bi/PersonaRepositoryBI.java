69c1c0f58b76b2c45efc79079289ca71f0cc6d4e
Santiago Parini <parinisantiago@gmail.com>
2018-08-13 22:08:16 -0300

    PersonaRepositoryBI

--- src/main/java/repository/bi/PersonaRepositoryBI.java
+++ src/main/java/repository/bi/PersonaRepositoryBI.java
@@ -6,6 +6,7 @@ import model.Persona;
 public interface PersonaRepositoryBI extends BaseRepositoryBI {
 
 	public Persona getPersona(String dni);
-	public Persona crearPersona(Persona persona);
+	public Persona crearPersona(Persona persona) throws Exception;
 	public Persona modificarPersona(Persona persona);
+	public Persona borrarPersona(int dni);
 }
