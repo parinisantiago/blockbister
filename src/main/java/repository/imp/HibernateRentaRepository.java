41e39c971cf2090758c36e06ce0c1a13c115b313
Santiago Parini <santiago.parini@globallogic.com>
2018-09-06 10:57:19 -0300

    Implementacion de borrar y devolver Renta en repositorio

--- src/main/java/repository/imp/HibernateRentaRepository.java
+++ src/main/java/repository/imp/HibernateRentaRepository.java
@@ -1,5 +1,7 @@
 package repository.imp;
 
+import java.util.Date;
+
 import model.Blockbister;
 import model.Renta;
 import repository.bi.RentaRepositoryBI;
@@ -16,6 +18,21 @@ public class HibernateRentaRepository  extends BaseHibernateRepository implement
 	public Renta rentar(Renta renta) {
 		Blockbister blockbister = (Blockbister) this.getSession().createQuery("FROM Blockbister B").list().iterator().next();
 		blockbister.addRenta(renta);
+		return renta;
+	}
+
+	@Override
+	public Renta devolver(long id) {
+		Renta renta = (Renta) this.getSession().createQuery("FROM Renta R WHERE R.idRenta = " + id).list().iterator().next();
+		renta.setDevolucion(new Date());
+		return renta;
+	}
+
+	@Override
+	public Renta cancelar(long id) {
+		Renta renta = (Renta) this.getSession().createQuery("FROM Renta R WHERE R.idRenta = " + id).list().iterator().next();
+		Blockbister blockbister = (Blockbister) this.getSession().createQuery("FROM Blockbister B").list().iterator().next();
+		blockbister.removeRenta(renta);
 		return renta;
 	}
 	
